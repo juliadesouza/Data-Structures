867e0ee06d045879b5b716921b68ac1e17f97bd6
Julia de Souza <julia.souza.santos@ibm.com>
2022-01-06 14:12:47 -0300

    added sparce search code

---
+++ 04 Sorting & Searching/codes/06-sparce-search.cpp
@@ -0,0 +1,75 @@
+// Given a sorted array of strings that is
+// interspersed with empty strings, write a method
+// to find the location of a given string.
+
+// Input
+// ["ai","","","bat","","","car","cat","","", "dog",""]
+// bat
+// Output
+// 4
+
+// using binary search
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int sparse_search(string a[], int n, string key)
+{
+
+    // sparse search
+    int s = 0;
+    int e = n - 1;
+    while (s <= e)
+    {
+        int mid = (s + e) / 2;
+        int mid_left = mid - 1;
+        int mid_right = mid + 1;
+        if (a[mid] == "")
+        {
+            while (1)
+            {
+                if (mid_left < s && mid_right > e)
+                {
+                    return -1;
+                }
+                else if (mid_right <= e && a[mid_right] != "")
+                {
+                    mid = mid_right;
+                    break;
+                }
+                else if (mid_left >= s && a[mid_left] != "")
+                {
+                    mid = mid_left;
+                    break;
+                }
+                mid_left--;
+                mid_right++;
+            }
+        }
+        if (a[mid] == key)
+        {
+            return mid;
+        }
+        else if (a[mid] > key)
+        {
+            e = mid - 1;
+        }
+        else
+        {
+            s = mid + 1;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    string arr[] = {"ai", "", "", "bat", "", "", "car", "cat", "", "", "dog", ""};
+    int n = 12;
+
+    string f;
+    cin >> f;
+    cout << sparse_search(arr, n, f) << endl;
+
+    return 0;
+}
