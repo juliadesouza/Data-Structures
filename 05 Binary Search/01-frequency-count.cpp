fe17a11147c2694dd3b3158f9efac36417943cd8
Julia de Souza <julia.souza.santos@ibm.com>
2022-01-06 14:43:20 -0300

    added frequency-count code

---
+++ 05 Binary Search/01-frequency-count.cpp
@@ -0,0 +1,86 @@
+// Given a sorted array which can contain repeated
+// elements, and an element K, we need to find the
+// frequency of a given element.
+// Input
+// [0,1,1,1,1,2,2,2,3,4,4,5,10]
+// key = 2
+// Output
+// 3
+
+// lower bound --> first occurence
+// upper bound --> last occurence
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int lower_bound(vector<int> arr, int key)
+{
+
+    int s = 0;
+    int e = arr.size() - 1;
+
+    int ans = -1;
+
+    while (s <= e)
+    {
+        int mid = (s + e) / 2;
+
+        if (arr[mid] == key)
+        {
+            ans = mid;
+            e = mid - 1;
+        }
+        else if (arr[mid] > key)
+        {
+            e = mid - 1;
+        }
+        else
+        {
+            s = mid + 1;
+        }
+    }
+    return ans;
+}
+int upper_bound(vector<int> arr, int key)
+{
+
+    int s = 0;
+    int e = arr.size() - 1;
+
+    int ans = -1;
+
+    while (s <= e)
+    {
+        int mid = (s + e) / 2;
+
+        if (arr[mid] == key)
+        {
+            ans = mid;
+            s = mid + 1;
+        }
+        else if (arr[mid] > key)
+        {
+            e = mid - 1;
+        }
+        else
+        {
+            s = mid + 1;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+
+    vector<int> arr = {0, 1, 1, 2, 3, 3, 3, 3, 3, 4, 5, 5, 5, 10};
+    int n = arr.size();
+
+    cout << upper_bound(arr, 3) - lower_bound(arr, 3) + 1 << endl;
+
+    //STL
+    cout << upper_bound(arr.begin(), arr.end(), 3) - lower_bound(arr.begin(), arr.end(), 3) << endl;
+
+    return 0;
+}
