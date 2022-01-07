ea38d6d1692ffad6cd8d1b86390273be81c21a34
Julia de Souza <julia.souza.santos@ibm.com>
2022-01-07 13:28:30 -0300

    added generating bracket code

---
+++ 06 Recursion/codes/03-generating-brackets.cpp
@@ -0,0 +1,49 @@
+// Write a function to generate all possible n pairs
+// of balanced brackets '(' and ')'
+
+// Input
+// 2
+// Output
+// ()()
+// (())
+
+// Rules
+// 1. closing brackets must come after opening brackets
+// 2. quantity of open brackets < N and quantity of open closing brackets < quantity of open brackets
+
+#include <iostream>
+using namespace std;
+
+void generateBrackets(string output, int n, int open, int close, int i)
+{
+    //base case
+    if (i == 2 * n)
+    {
+        cout << output << endl;
+        return;
+    }
+
+    //open
+    if (open < n)
+    {
+
+        generateBrackets(output + '(', n, open + 1, close, i + 1);
+    }
+
+    //closing
+    if (close < open)
+    {
+        generateBrackets(output + ')', n, open, close + 1, i + 1);
+    }
+}
+
+int main()
+{
+
+    string output;
+    int n;
+    cin >> n;
+    generateBrackets(output, n, 0, 0, 0);
+
+    return 0;
+}
