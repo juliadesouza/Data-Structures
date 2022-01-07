4253ab11d8417da67554d67a36c434b3e088e1ff
Julia de Souza <julia.souza.santos@ibm.com>
2022-01-07 14:05:56 -0300

    added permutations code - TODO

---
+++ 06 Recursion/codes/05-permutations.cpp
@@ -0,0 +1,25 @@
+// Given an string s, find all permutations of the string.
+// Input
+// abc
+// Output
+// abc, acb, bac, bca, bca, cab, cba
+
+// while going back, we need to restore our original
+// array: BACKTRACKING
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void permute(string a, int i)
+{
+}
+
+int main()
+{
+
+    string s;
+    cin >> s;
+    permute(s, 1);
+    return 0;
+}
