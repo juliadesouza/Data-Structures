1ae9e250f908bfa7b977ee06700b7b6223fba759
Julia de Souza <julia.souza.santos@ibm.com>
2022-01-11 10:34:20 -0300

    added statement to the array of products exercise

--- 01 Arrays & Vectors/exercises/array-products.cpp
+++ 01 Arrays & Vectors/exercises/array-products.cpp
@@ -1,3 +1,16 @@
+// Array Products
+// Implement a function that takes a vector of integers, and returns a vector of the same
+// length, where each element in the output array is equal to the product of every other number
+// in the input array. Solve this problem without using division.
+// In other words, the value at output[i] is equal to the product of every number in the input
+// array other than input[i]. You can assume that answer can be stored inside int datatype and
+// no-overflow will occur due to products.
+// Sample Input
+// Both inputs and outputs are vectors.
+// {1,2,3,4,5}
+// Sample Output
+// {120, 60, 40, 30, 24}
+
 #include <iostream>
 #include <vector>
 
