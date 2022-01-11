76169454188448613e497df5b055bbd35b002f8b
Julia de Souza <julia.souza.santos@ibm.com>
2022-01-11 10:32:16 -0300

    removed duplicated minimum difference exercise

--- 01 Arrays & Vectors/exercises/minimum-difference.cpp
+++ 01 Arrays & Vectors/exercises/minimum-difference.cpp
@@ -10,28 +10,36 @@
 //      17,19
 
 #include <vector>
-#include <iostream>
-#include <utility>
+#include <algorithm>
+#include <climits>
+#include <cstdlib>
 using namespace std;
 
-pair<int, int> minDifference(vector<int> a, vector<int> b)
+pair<int, int> minDifference(vector<int> a1, vector<int> a2)
 {
-    //Complete this method
-    int n = a.size();
-    int m = b.size();
-    
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            int diff = a[i] - b[]
+
+    pair<int, int> result;
+    int minDiff = INT_MAX;
+
+    sort(a2.begin(), a2.end());
+    for (int num : a1)
+    {
+        auto idx = lower_bound(a2.begin(), a2.end(), num) - a2.begin();
+
+        if (idx >= 1 && abs(num - a2[idx - 1]) < minDiff)
+        {
+            minDiff = abs(num - a2[idx - 1]);
+            result.first = a2[idx - 1];
+            result.second = num;
+        }
+
+        if (idx != a2.size() and a2[idx] - num < minDiff)
+        {
+            minDiff = a2[idx] - num;
+            result.first = num;
+            result.second = a2[idx];
         }
     }
-    
-}
 
-int main()
-{
-    vector<int> v = {-1, 2, 3, 4, -2, 6, -8, 3};
-    cout << maxSubarraySum(v) << endl;
-
-    return 0;
+    return result;
 }
